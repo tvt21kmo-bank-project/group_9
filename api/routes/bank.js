0c257f8dc418d9b0a0e34fd06bfa487bc224c67a
unknown <t0moan02@students.oamk.fi>
2021-12-07 22:24:54 +0200

    kokeillaan tätä tietokantaa

--- api/routes/bank.js
+++ api/routes/bank.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bank = require('../models/bank_model');
 
-router.post('/debit_transfer', function(request, response){
+router.post('/debit_tilisiirto', function(request, response){
     bank.debit(request.body, function(err, dbResult){
         if(err){
             response.json(err);
@@ -14,7 +14,7 @@ router.post('/debit_transfer', function(request, response){
 }
 );
 
-router.post('/credit_transfer', function(request, response){
+router.post('/credit_tilisiirto', function(request, response){
     bank.credit(request.body, function(err, dbResult){
         if(err){
             response.json(err);
